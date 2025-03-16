9d22614e6069a26f0b8cb7fb4731b39e36b309ca
Kali-OAP <luisbrice417@gmail.com>
2025-03-16 09:22:40 -0500

    Ya la termine, lee el comentario que esta en el archivo

--- Calcuator.py
+++ Calcuator.py
@@ -1,1 +1,18 @@
-print ("test")
+
+valor1 = int(input("Ingresa un digito: "))
+operacion = input("Ingrese la operacion que desea realizar: ")
+valor2 = int(input("Ingrese otro digito: "))
+
+if operacion == "+":
+    print("El resultado es: ", valor1 + valor2)
+elif operacion == "-":
+    print("El resultado es: ", valor1 - valor2)
+elif operacion == "*":
+    print("El resultado es: ", valor1 * valor2)
+elif operacion == "/":
+    print("El resultado es: ", valor1 / valor2)
+else:
+    print("Operacion no valida")
+
+    # Esto termino siendo mas simple que lo que esperaba jajaja, cuando leas esto me avisas, para proceder a desarrollar un juego muy simple XD
+    
