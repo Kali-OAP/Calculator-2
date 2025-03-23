4c42381e2cc038322782aed6147aeb21b5dda690
Matiatsu <redrobanmatias416@gmail.com>
2025-03-22 21:19:45 -0400

    se corrigio los errores de las letras

--- Calcuator.py
+++ Calcuator.py
@@ -1,18 +1,20 @@
 
-valor1 = int(input("Ingresa un digito: "))
-operacion = input("Ingrese la operacion que desea realizar: ")
-valor2 = int(input("Ingrese otro digito: "))
+def calculator():
+    try:
+        valor1 = int(input("Ingresa un digito: "))
+        operacion = input("Ingrese la operacion que desea realizar: ")
+        valor2 = int(input("Ingrese otro digito: "))
 
-if operacion == "+":
-    print("El resultado es: ", valor1 + valor2)
-elif operacion == "-":
-    print("El resultado es: ", valor1 - valor2)
-elif operacion == "*":
-    print("El resultado es: ", valor1 * valor2)
-elif operacion == "/":
-    print("El resultado es: ", valor1 / valor2)
-else:
-    print("Operacion no valida")
 
-    # Esto termino siendo mas simple que lo que esperaba jajaja, cuando leas esto me avisas, para proceder a desarrollar un juego muy simple XD
-    
+        if operacion == "+":
+            print("El resultado es: ", valor1 + valor2)
+        elif operacion == "-":
+            print("El resultado es: ", valor1 - valor2)
+        elif operacion == "*":
+            print("El resultado es: ", valor1 * valor2)
+        elif operacion == "/":
+            print("El resultado es: ", valor1 / valor2)
+    except ValueError:
+        print("ingrese un valor no una letra")
+
+calculator()
